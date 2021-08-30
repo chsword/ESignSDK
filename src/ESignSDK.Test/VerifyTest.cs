beaffb0122360e3ef34d3e8bbd2a09a483dad5ea
chsword <chsword@126.com>
2021-08-30 18:24:47 +0800

    + 验签Test

---
+++ src/ESignSDK.Test/VerifyTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace ESignSDK.Test
+{
+    [TestClass]
+    public class VerifyTest:BaseUnitTest
+    {
+        [TestMethod]
+        public void DocumentVerifyTest()
+        {
+            var flow = _client.SignFlowDocumentGet("").Result;
+            Console.WriteLine(JsonConvert.SerializeObject(flow));
+         
+            var result = _client.DocumentsVerify(flow.Data.Docs.FirstOrDefault().FileId, "").Result;
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+ 
+        }
+
+    }
+}
